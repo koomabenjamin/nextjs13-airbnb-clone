3136fd7a3b77773c5a4b97ea8e609b4c64dc3fb2
Project Code <projectcode@Projects-iMac.local>
2023-05-23 11:07:47 +0300

    Implement the menuitem and the toggle functionality

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,5 +1,16 @@
+'use client';
+import Image from "next/image";
+
 const Avatar = () => {
-  return (  );
+  return ( 
+    <Image
+      className="rounded-full"
+      height={30}
+      width={30}
+      alt="avatar"
+      src={"/placeholder.jpg"}
+    />
+  );
 }
  
 export default Avatar;
