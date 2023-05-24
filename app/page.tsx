f28687a6aa623d439c8a90a1102a226c501c1a46
Project Code <projectcode@Projects-iMac.local>
2023-05-24 09:50:49 +0300

    Formulate db structure and connect app to mongodb atlast

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 import Image from 'next/image'
+import { BiLoaderAlt } from 'react-icons/bi';
 
 export default function Home() {
   return (
-    <div className='text-rose-500 text-2xl'>Hello Airbnb</div>
+    <div
+      className='
+        text-rose-500
+        text-2xl'
+    >
+      <span>Loading...</span>
+      <BiLoaderAlt/>
+    </div>
   )
 }
